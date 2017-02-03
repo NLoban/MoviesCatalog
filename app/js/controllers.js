abcd728da7a5fe13660d160acef796e671adfc55
Loban <n.loban@itransition.com>
2017-02-03 17:37:35 +0300

    First commit

---
+++ app/js/controllers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var moviesApp = angular.module("moviesApp", []);
+
+moviesApp.controller('mainCtrl', ['$http', function($http) {
+	var self = this;
+	$http({
+		method: 'GET', 
+		url: 'https://api.themoviedb.org/4/list/1?api_key=8ca72ae90f15b5d823a990ab310a5160'
+	}).then(function(response) {
+          self.status = response.status;
+          self.cinemas  = response.data.results;
+        }, function(response) {
+          self.cinemas = response.data.result || 'Request failed';
+          self.status = response.status;
+      });
+        console.log(this.cinemas);
+        // 'https://image.tmdb.org/t/p/w500/'
+
+    this.getPath = function(imagePath){
+    	return 'https://image.tmdb.org/t/p/w500' + imagePath;
+    };
+}]);
