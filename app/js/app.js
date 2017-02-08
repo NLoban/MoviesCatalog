4687885d01b08c664db2979154be3ce44ee8df28
Loban <n.loban@itransition.com>
2017-02-08 12:42:15 +0300

    angular-translate, format, filter by name

--- app/js/app.js
+++ app/js/app.js
@@ -1,58 +1,56 @@
-var moviesApp = angular.module("moviesApp", ['ngRoute', 'ngResource']);
+
+var moviesApp = angular.module("moviesApp", ['ngRoute', 'ngResource', 'pascalprecht.translate']);
 
 moviesApp.component("movies", {
-  templateUrl: 'template/movies.html',
-  controller: 'MoviesCtrl',
-  controllerAs: 'MoviesCtrl'
+    templateUrl: 'template/movies.html',
+    controller: 'MoviesCtrl',
+    controllerAs: 'MoviesCtrl'
 });
 
 moviesApp.component("movieDetails", {
-  templateUrl: 'template/movie-details.html',
-  controller: 'MovieDetailsCtrl'
+    templateUrl: 'template/movie-details.html',
+    controller: 'MovieDetailsCtrl'
 });
 
-moviesApp.config(['$locationProvider', '$routeProvider',
-  function config($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
+moviesApp.config(['$locationProvider', '$routeProvider', '$translateProvider',
+    function config($locationProvider, $routeProvider, $translateProvider) {
+        $locationProvider.hashPrefix('!');
 
-    $routeProvider.
-      when('/:type/:category', {
-        template: '<movies></movies>'
-      }).
-      when('/:type/details/:movieId', {
-        template: '<movie-details></movie-details>'
-      }).
-      otherwise('/');
-  }
+        $routeProvider.
+            when('/:type/:category', {
+                template: '<movies></movies>'
+            }).
+            when('/:type/details/:movieId', {
+                template: '<movie-details></movie-details>'
+            }).
+            otherwise('/');
+
+        $translateProvider.useStaticFilesLoader({
+            prefix: '/resources/translation_',
+            suffix: '.json'
+        });
+
+        $translateProvider.preferredLanguage('en');
+    }
 ]);
 
-moviesApp.factory('DataInitializer', ['$http', '$q', function($http, $q) {
+moviesApp.factory('DataInitializer', ['$http', '$q', function ($http, $q) {
 
-  function getItems(url) {
-     var deferred = $q.defer();
-    $http({
-      method: 'GET', 
-      url: url
-    }).then(function(response) {
-        deferred.resolve(response.data.results === undefined ? response.data : response.data.results);
-      }, function(response) {
-        deferred.reject('Request failed');
-    });
-    return deferred.promise; 
-  }
-
-  return  {
-    getItems: getItems
-  }
-   
-}] )
-
-moviesApp.service('translationService', function($resource) {  
-    this.getTranslation = function($scope, language) {
-        var languageFilePath = 'resources/translation_' + language + '.json';
-        console.log(languageFilePath);
-        $resource(languageFilePath).get(function (data) {
-            $scope.translation = data;
+    function getItems(url) {
+        var deferred = $q.defer();
+        $http({
+            method: 'GET',
+            url: url
+        }).then(function (response) {
+            deferred.resolve(response.data.results === undefined ? response.data : response.data.results);
+        }, function (response) {
+            deferred.reject('Request failed');
         });
-    };
-});
+        return deferred.promise;
+    }
+
+    return {
+        getItems: getItems
+    }
+
+}])
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,43 +1,47 @@
 'use strict';
 
-moviesApp.controller('MainCtrl', ['translationService', function (translationService){
+moviesApp.controller('MainCtrl', ['$translate', '$route', function ($translate, $route) {
   var self = this;
-  this.translate = function(language){
-      translationService.getTranslation(self, language === undefined ? self.selectedLanguage : language);
-      self.selectedLanguage = language === undefined ? self.selectedLanguage : language;
+  this.changeLanguage = function (language) {
+    $translate.use(language);
+    self.selectedLanguage = language;
+    $route.reload();
   };
 
   this.selectedLanguage = 'en';
-  this.translate();  
 }]);
 
-moviesApp.controller('MoviesCtrl', ['$http', '$routeParams', '$q', 'DataInitializer', 
-function($http, $routeParams, $q, DataInitializer) {
-	var self = this;
-  var url = 'https://api.themoviedb.org/3/' 
-            + $routeParams.type 
-            + '/' 
-            + $routeParams.category 
-            + '?api_key=8ca72ae90f15b5d823a990ab310a5160';
+moviesApp.controller('MoviesCtrl', ['$http', '$routeParams', '$q', 'DataInitializer', '$translate',
+  function ($http, $routeParams, $q, DataInitializer, $translate) {
+    var self = this;
+    var selectedLanguage = $translate.use();
+    var url = 'https://api.themoviedb.org/3/'
+      + $routeParams.type
+      + '/'
+      + $routeParams.category
+      + '?api_key=8ca72ae90f15b5d823a990ab310a5160&language='
+      + selectedLanguage;
 
-  DataInitializer.getItems(url).then(function (result) {
-    self.data = result;
-  });
-  self.type = $routeParams.type;
-}]);
+    DataInitializer.getItems(url).then(function (result) {
+      self.data = result;
+    });
+    self.type = $routeParams.type;
+  }]);
 
-moviesApp.controller('MovieDetailsCtrl', ['$http', '$routeParams', '$q', 'DataInitializer', 
-function($http, $routeParams, $q, DataInitializer) {
-	var self = this;
-  var url = 'https://api.themoviedb.org/3/' 
-            + $routeParams.type 
-            + '/' 
-            + $routeParams.movieId 
-            + '?api_key=8ca72ae90f15b5d823a990ab310a5160&language=en-US';
-  DataInitializer.getItems(url).then(function (result) {
-    self.data = result;
-  });
-}]);
+moviesApp.controller('MovieDetailsCtrl', ['$http', '$routeParams', '$q', 'DataInitializer', '$translate',
+  function ($http, $routeParams, $q, DataInitializer, $translate) {
+    var self = this;
+    var selectedLanguage = $translate.use();
+    var url = 'https://api.themoviedb.org/3/'
+      + $routeParams.type
+      + '/'
+      + $routeParams.movieId
+      + '?api_key=8ca72ae90f15b5d823a990ab310a5160&language='
+      + selectedLanguage;
+    DataInitializer.getItems(url).then(function (result) {
+      self.data = result;
+    });
+  }]);
 
     // https://api.themoviedb.org/3/discover/movie?api_key=8ca72ae90f15b5d823a990ab310a5160
 		// url: 'https://api.themoviedb.org/4/list/1?api_key=8ca72ae90f15b5d823a990ab310a5160'
